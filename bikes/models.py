bfc1115f2ea04af110e83b4ad3b462d9a5039f21
Rinaldo-Neto <rinaldoneto15@gmail.com>
2026-02-06 21:20:58 -0300

    Adicionando o Model de Bike

--- bikes/models.py
+++ bikes/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Bike(models.Model):
+    id = models.AutoField(primary_key=True)
+    model = models.CharField(max_length=200)
+    brand = models.CharField(max_length=200)
+    factory_year = models.IntegerField(blank=True, null=True)
+    model_year = models.IntegerField(blank=True, null=True)
+    valuer = models.FloatField(blank=True, null=True)
