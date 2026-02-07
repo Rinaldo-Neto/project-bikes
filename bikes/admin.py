3c81cd923822c71538b3c5625c2b9c4d0abd9747
Rinaldo-Neto <rinaldoneto15@gmail.com>
2026-02-06 21:51:38 -0300

    add/config  do Model Brand e do BrandAdmin

--- bikes/admin.py
+++ bikes/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
-from bikes.models import Bike
+from bikes.models import Bike,Brand
 
-# Register your models here.
+
+class BrandAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+
 class BikeAdmin(admin.ModelAdmin):
     list_display = ('model', 'brand', 'factory_year', 'model_year', 'value',)
     search_fields = ('model, brand',)
 
+
+admin.site.register(Brand, BrandAdmin)
 admin.site.register(Bike, BikeAdmin)
